acf136951dfb5afd5be8e98b005d47ab3cabc180
ryzenn <kevatrajat29@gmail.com>
2025-11-08 21:33:36 +0530

    ok

---
+++ build.gradle.kts
@@ -0,0 +1,9 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.jetbrains.kotlin.android) apply false
+    alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.google.gms.google.services) apply false
+    kotlin("jvm") version "2.1.0"
+    id("com.google.devtools.ksp") version "2.1.0-1.0.29" apply false
+}
